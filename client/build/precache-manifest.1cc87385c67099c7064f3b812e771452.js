9ccf77832cb1653d0f0144301d0bf8dc86c37e66
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-03-12 11:43:06 +0100

    Fix /build problems

---
+++ client/build/precache-manifest.1cc87385c67099c7064f3b812e771452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de043de0a309c03a73455b0433cd85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0383317716a3776cdf7",
+    "url": "/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "d51a59546c067800bee1",
+    "url": "/static/css/main.4bbb1106.chunk.css"
+  },
+  {
+    "revision": "f0383317716a3776cdf7",
+    "url": "/static/js/2.9c5930c1.chunk.js"
+  },
+  {
+    "revision": "d51a59546c067800bee1",
+    "url": "/static/js/main.cdc5626b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
